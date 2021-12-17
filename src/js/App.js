58866c30564f74f038cae6cef6c379aeab8623bb
LENIN309 <yairca734@gmail.com>
2021-12-17 04:01:25 +0000

    React TODO

---
+++ src/js/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+import TodoList from './components/TodoList';
+
+function App(){
+    return(
+        <div className='todo-app'>
+            <TodoForm/>
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
+
---
+++ src/js/components/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import TodoForm from './TodoForm'
+
+function Todo() {
+    const [edit, setEdit] = useState({
+        id: null, 
+        value: ''
+    })
+
+    return todos.map((todo, index) =>(
+        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}></div>
+
+    ))
+}
+
+export default Todo
