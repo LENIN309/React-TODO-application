a73923c913684ff0596fff66fcd373bc6eea4978
LENIN309 <yairca734@gmail.com>
2021-12-19 00:36:43 +0000

    review

--- src/js/components/TodoForm.js
+++ src/js/components/TodoForm.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef,current.focus()        
+    })
+
 
     const handlechange = e => {
-        
         setInput(e.target.value);
-
-    }
+    };
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -22,6 +28,7 @@ function TodoForm(props) {
     
     return (
         <form className='todo-form' onSubmit={handleSubmit}>
+        
         <input
         type='text'
         placeholder='Add a todo'
@@ -29,6 +36,7 @@ function TodoForm(props) {
         name='text'
         className='todo-input'
         onChange={handlechange}
+        ref={inputRef}
         />
         <button className="todo-button">Add Todo</button>
         </form>
